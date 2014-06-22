c45504fc6b52a1855a703f2c3fa951815b040120
7korobi <7korobi@gmail.com>
2014-06-23 01:52:29 +0900

    file log link.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,7 +7,7 @@ class StoriesController < ApplicationController
         only(%w[_type folder vid name rating options upd vpl type card]).
         sort_by{|o|[o.folder, - o.vid]}.
         map do |story|
-          story[:file] = message_file_path(story)
+          story[:file] = "#{URL[:file]}/stories/#{story.id}.html"
           story[:link] = messages_path(story, 0) + "#mode=info_all_open"
           story
         end
