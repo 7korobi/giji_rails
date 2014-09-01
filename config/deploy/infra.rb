61ac12fc0688d7045490894ef80da0a2d303009c
7korobi <7korobi@gmail.com>
2014-09-02 02:23:48 +0900

    capistrano fix.

--- config/deploy/infra.rb
+++ config/deploy/infra.rb
@@ -1,11 +1,5 @@
 set :rails_envs, ["production"]
 
-server 'utage.family.jp',
-  port: 2450,
-  user: "7korobi",
-  roles: [:app, :resque, :queue],
-  rails_env: "production"
-
 server '183.181.24.203',
   port: 3843,
   user: "7korobi",
