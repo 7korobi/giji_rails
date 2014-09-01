55bea96c56f258a85cd0b8b63d9e73f3e92dcef6
7korobi <7korobi@gmail.com>
2014-09-02 02:29:07 +0900

    bye public

--- public/assets-show-fix/boot.js
+++
@@ -1,5 +0,0 @@
-jQuery(function() {
-  FixedBox.push(angular.element, 0, 1, '#topviewer');
-  FixedBox.push(angular.element, 1, -1, '#sayfilter');
-  return FixedBox.push(angular.element, -12, -1, '#buttons');
-});
